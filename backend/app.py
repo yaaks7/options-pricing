13cea31c84f7f434c55b2a1e657f281d09bbe25a
yaaks7 <yanis.aksas@gmail.com>
2024-10-20 16:18:27 -0400

    Preparing deployement

--- backend/app.py
+++ backend/app.py
@@ -37,11 +37,12 @@ app = FastAPI()
 # Configuration of middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 class OptionData(BaseModel):
     time_to_maturity: float
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,51 +1,52 @@
 //src/services/api.js
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 // Function for Black-Scholes
 export const fetchBlackScholesPrice = async (data) => {
-  const response = await axios.post('http://localhost:8000/price/blackscholes', data);
+  const response = await axios.post(`${BASE_URL}/price/blackscholes`, data);
   return response.data;
 };
 
 // Function for Binomial
 export const fetchBinomialPrice = async (data) => {
-  const response = await axios.post('http://localhost:8000/price/binomial', data);
+  const response = await axios.post(`${BASE_URL}/price/binomial`, data);
   return response.data;
 };
 
 // Function for Monte Carlo
 export const fetchMonteCarloPrice = async (data) => {
-  const response = await axios.post('http://localhost:8000/price/montecarlo', data);
+  const response = await axios.post(`${BASE_URL}/price/montecarlo`, data);
   return response.data;
 };
 
 // Function for Neural Network
 export const fetchNeuralNetworkPrice = async (data) => {
-  const response = await axios.post('http://localhost:8000/price/neuralnetwork', data);
+  const response = await axios.post(`${BASE_URL}/price/neuralnetwork`, data);
   return response.data;
 };
 
 // Function for Greeks
 export const fetchGreeks = async (data) => {
-  const response = await axios.post('http://localhost:8000/greeks', data);
+  const response = await axios.post(`${BASE_URL}/greeks`, data);
   return response.data;
 };
 
 // Function for Heatmap P&L
 export const fetchHeatmapPnl = async (data) => {
-  const response = await axios.post('http://localhost:8000/heatmap_pnl/', data);
+  const response = await axios.post(`${BASE_URL}/heatmap_pnl/`, data);
   return response.data;
 };
 
 // Function for Option Sensitivity
 export const fetchOptionSensitivity = async (data) => {
-    const response = await axios.post('http://localhost:8000/option_sensitivity/', data);
+    const response = await axios.post(`${BASE_URL}/option_sensitivity/`, data);
     return response.data;
 };
 
 // Function for Greeks Sensitivity
 export const fetchGreeksSensitivity = async (data) => {
-  const response = await axios.post('http://localhost:8000/greeks_sensitivity/', data);
+  const response = await axios.post(`${BASE_URL}/greeks_sensitivity/`, data);
   return response.data;
 };
 
